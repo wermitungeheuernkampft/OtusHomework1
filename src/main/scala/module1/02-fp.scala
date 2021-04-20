5603c29756e2548e99692f10b125d1b37f224228
wermitungeheuernkampft <wermitungeheuernkampft@gmail.com>
2021-04-21 01:58:57 +0300

    hw1

--- src/main/scala/module1/02-fp.scala
+++ src/main/scala/module1/02-fp.scala
@@ -90,7 +90,37 @@ object recursion {
    *
    */
 
-   def fib(n: Int): Int = fib(n -1) + fib(n - 2)
+    // как через циклы и хвостовую делать - пока не разобрался...
+
+  /*def fib(n: Int): Int = {
+    var _n: Int = 0
+    var i: Int = 1
+    while (i >= n) {
+      _n = _n + i+1
+      i = i + 1
+    }
+    _n
+  }
+*/
+
+  // рекурсия
+  def fib2 (n: Int): Int = {
+    if (n == 0) 0
+    else if (n == 1) 1
+    else fib2(n-1) + fib2(n-2)
+  }
+
+  /*
+  @tailrec
+  def fib3 (n: Int): Int = {
+    def sum (a: Int, b: Int): Int = {
+      if (a == 0) 0
+      else if (a == 1) 1
+      else sum
+    }
+    sum (n-1, n-2)
+  }
+*/
 
 }
 
@@ -149,12 +179,6 @@ object hof{
       case Option.None => true
     }
 
-    def get: A = this match {
-      case Option.Some(v) => v
-      case Option.None => throw new Exception("Get on empty list")
-    }
-
-
     def getOrElse[B >: A](b: B): B = this match {
       case Option.Some(v) => v
       case Option.None => b
@@ -165,56 +189,97 @@ object hof{
       case Option.None => Option.None
     }
 
-    def flatMap[B](f: A => Option[B]): Option[B] = ???
+    // праильно ли реализован flatmap ???
+
+    def flatMap[B](f: A => Option[B]): Option[B] = this match {
+      case Option.Some(v) => f(v)
+      case Option.None => Option.None
+    }
 
     // val i : Option[Int]  i.map(v => v + 1)
-
 
     def f(x: Int, y: Int): Option[Int] =
       if(y == 0) Option.None
       else Option.Some(x / y)
 
+    /**
+     *
+     * Реализовать метод printIfAny, который будет печатать значение, если оно есть
+     */
 
+    def printIfAny(): Unit = this match {
+      case Option.Some(_) => println(_)
+      case Option.None => throw new Exception("Nothing to print")
+    }
+
+    /**
+     *
+     * реализовать метод orElse который будет возвращать другой Option, если данный пустой
+     */
+
+      // или
+    def getOrElse [B] (elseOption: Option[B]) : Option[_] = this match {
+      case Option.Some(v) => Option.Some(v)
+      case Option.None => elseOption
+    }
+
+      // или
+    def getOrElse2 [B >: A] (elseOption: Option[B]) : Option[B] = this match {
+      case Option.Some(v) => Option.Some(v)
+      case Option.None => elseOption
+    }
+
+    /**
+     *
+     * Реализовать метод isEmpty, который будет возвращать true если Option не пуст и false в противном случае
+     */
+
+    def isEmpty2 : Boolean = this match {
+      case Option.Some(_) => true
+      case Option.None => false
+    }
+
+    /**
+     *
+     * Реализовать метод get, который будет возвращать значение
+     */
+
+    def get: A = this match {
+      case Option.Some(v) => v
+      case Option.None => throw new Exception("Get on empty list")
+    }
+
+    /**
+     *
+     * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
+     */
+
+    def zip [B] (otherOption: Option[B]): Option[(Option[A], Option [B])] = this match {
+      case Option.Some(v) => otherOption match {
+        case Option.Some(w) => Option.Some(Option.Some(v), Option.Some(w))
+        case Option.None => Option.Some(Option.Some(v), Option.None)
+      }
+      case Option.None => otherOption match {
+        case Option.Some(w) => Option.Some(Option.None, Option.Some(w))
+        case Option.None => Option.Some(Option.None, Option.None)
+      }
+    }
+
+    /**
+     *
+     * Реализовать метод filter, который будет возвращать не пустой Option
+     * в случае если исходный не пуст и предикат от значения = true
+     */
+
+    def filter (f: A => Boolean): Option[A] = this match {
+      case Option.Some(v) if f(v) => Option.Some(v)
+      case Option.None => Option.None
+      }
   }
 
    object Option{
      case class Some[A](v: A) extends Option[A]
      case object None extends Option[Nothing]
    }
-
-
-  /**
-   *
-   * Реализовать метод printIfAny, который будет печатать значение, если оно есть
-   */
-
-  /**
-   *
-   * реализовать метод orElse который будет возвращать другой Option, если данный пустой
-   */
-
-
-  /**
-   *
-   * Реализовать метод isEmpty, который будет возвращать true если Option не пуст и false в противном случае
-   */
-
-
-  /**
-   *
-   * Реализовать метод get, который будет возвращать значение
-   */
-
-  /**
-   *
-   * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
-   */
-
-
-  /**
-   *
-   * Реализовать метод filter, который будет возвращать не пустой Option
-   * в случае если исходный не пуст и предикат от значения = true
-   */
 
  }
